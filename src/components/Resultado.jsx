2a146e924f562db3e03d6d8ff6f9c4a954a7ba71
eliasbenavides <elias02d2@gmail.com>
2022-06-12 20:17:12 -0600

    first commit

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Result = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+`;
+
+const Texto = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Resultado = ({ resultado }) => {
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
+    resultado;
+
+  return (
+    <Result>
+      <Precio>
+        El precio es de: <span>{PRICE}</span>
+      </Precio>
+      <Texto>
+        Precio más alto del día: <span>{HIGHDAY}</span>
+      </Texto>
+      <Texto>
+        Precio más bajo del día: <span>{LOWDAY}</span>
+      </Texto>
+      <Texto>
+        Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
+      </Texto>
+      <Texto>
+        Última Actualización: <span>{LASTUPDATE}</span>
+      </Texto>
+    </Result>
+  );
+};
+
+export default Resultado;
